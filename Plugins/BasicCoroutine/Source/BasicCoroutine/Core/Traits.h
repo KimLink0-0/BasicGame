a5540bcf87d0e1fe685de927959a216815705eb0
KimLink <Linkedcirrus93@gmail.com>
2026-01-30 20:58:27 +0900

    [Update(BasicCoroutine)] Add coroutine traits specialization
    - Add `Traits.h` to specialize `std::coroutine_traits` for `TCoroTask<T>`.
    - Link `TCoroTask` to `Coro::Private::TPromise` via the traits specialization.

---
+++ Plugins/BasicCoroutine/Source/BasicCoroutine/Core/Traits.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+#include "BasicCoroutine/Core/Private.h"
+#include "BasicCoroutine/Core/Promise.h"
+
+template<typename T, typename... Args>
+struct std::coroutine_traits<TCoroTask<T>, Args...>
+{
+	using promise_type = Coro::Private::TPromise<T>;
+};
